1f59bb2c5b5785e25f64a6c8861a461c1bbd87d8
Kent Watsen <kent@watsen.net>
2016-02-25 20:41:53 -0500

    This is -08

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,21 +1,16 @@
-#pyang -p ../ -f tree --tree-path=/netconf-server/session-options ../ietf-netconf-server\@*.yang > session-options-tree.txt 
-#pyang -p ../ -f tree --tree-path=/netconf-server/call-home ../ietf-netconf-server\@*.yang > call-home-tree.txt 
-#pyang -p ../ -f tree --tree-path=/netconf-server/listen ../ietf-netconf-server\@*.yang > listen-tree.txt 
-#pyang -p ../ -f tree --tree-path=/netconf-server/ssh ../ietf-netconf-server\@*.yang > ssh-tree.txt 
-#pyang -p ../ -f tree --tree-path=/netconf-server/tls ../ietf-netconf-server\@*.yang > tls-tree.txt 
 
-#pyang -p ../ -f tree --tree-path=/restconf-server/listen ../ietf-restconf-server\@*.yang > restconf-listen-tree.txt 
-#pyang -p ../ -f tree --tree-path=/restconf-server/call-home ../ietf-restconf-server\@*.yang > restconf-call-home-tree.txt 
-#pyang -p ../ -f tree --tree-path=/restconf-server/client-cert-auth ../ietf-restconf-server\@*.yang > restconf-client-cert-auth-tree.txt 
+# for keychain-based solution
 
+pyang -p ../ -f tree ../ietf-keychain@*.yang > ietf-keychain-tree.txt.tmp
+pyang -p ../ -f tree ../ietf-ssh-server\@*.yang > ietf-ssh-server-tree.txt.tmp
+pyang -p ../ -f tree ../ietf-tls-server\@*.yang > ietf-tls-server-tree.txt.tmp
+pyang -p ../ -f tree ../ietf-netconf-server\@*.yang > ietf-netconf-server-tree.txt.tmp
+pyang -p ../ -f tree ../ietf-restconf-server\@*.yang > ietf-restconf-server-tree.txt.tmp
 
-# Current Solution
-pyang -p ../ -f tree ../ietf-netconf-server\@*.yang > ietf-netconf-server-tree.txt
-pyang -p ../ -f tree ../ietf-restconf-server\@*.yang > ietf-restconf-server-tree.txt
+fold -w 71 ietf-keychain-tree.txt.tmp > ietf-keychain-tree.txt
+fold -w 71 ietf-ssh-server-tree.txt.tmp > ietf-ssh-server-tree.txt
+fold -w 71 ietf-tls-server-tree.txt.tmp > ietf-tls-server-tree.txt
+fold -w 71 ietf-netconf-server-tree.txt.tmp > ietf-netconf-server-tree.txt
+fold -w 71 ietf-restconf-server-tree.txt.tmp > ietf-restconf-server-tree.txt
 
-# Issue #49
-pyang -p ../ -f tree ../ietf-keychain@*.yang > ietf-keychain-tree.txt
-pyang -p ../ -f tree ../ietf-ssh-server-fake\@*.yang > ietf-ssh-server-tree.txt
-pyang -p ../ -f tree ../ietf-tls-server-fake\@*.yang > ietf-tls-server-tree.txt
-pyang -p ../ -f tree ../ietf-netconf-server-new\@*.yang > ietf-netconf-server-new-tree.txt
-pyang -p ../ -f tree ../ietf-restconf-server-new\@*.yang > ietf-restconf-server-new-tree.txt
+rm *-tree.txt.tmp
--- refs/validate.sh
+++ refs/validate.sh
@@ -3,21 +3,37 @@
 OUT=$2.tmp
 
 cat > $OUT <<EOF
-<rpc-reply message-id="101" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
-  <data>
+<data xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
 EOF
 
 cat $2 >> $OUT
 
 cat >> $OUT <<EOF
-  </data>
-</rpc-reply>
+</data>
 EOF
 
-yang2dsdl -t get-reply ../$1
-
+echo
+echo "converting yang to dsdl..."
+yang2dsdl -t data ../$1
 basename=`echo $1 | sed 's/\@.*//'`
-xmllint --noout --relaxng $basename-get-reply.rng  $OUT 
+echo
+
+echo
+echo "syntactic only validation (via xmlint)..."
+xmllint --noout --relaxng $basename-data.rng  $OUT 
+echo
+
+echo
+echo "syntactic only validation (via jing-trang)..."
+#JING BROKE/Users/kwatsen/Juniper/version-control-servers/github/jing-trang/jing $basename-data.rng  $OUT
+java -classpath "/Users/kwatsen/Juniper/version-control-servers/github/jing-trang/build/jing.jar" "com.thaiopensource.relaxng.util.Driver" $basename-data.rng  $OUT
+echo
+
+echo
+echo "syntactic + semantic validation:"
+#JING BROKE yang2dsdl -s -j -t data -b $basename -v $OUT  
+yang2dsdl -s -t data -b $basename -v $OUT
+echo
 
 rm $OUT
 rm *.rng *.dsrl *.sch
